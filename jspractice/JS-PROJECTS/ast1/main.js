9d475cc9dca96f85e9812eacd4e850176d908cfc
keshsu <keshab.bhadel.5@gmail.com>
2019-11-27 15:46:33 +0545

    js image slideradded

--- jspractice/JS-PROJECTS/ast1/main.js
+++ jspractice/JS-PROJECTS/ast1/main.js
@@ -1,83 +1,146 @@
-var index=0;
-var images= [];
+var index=1;
+
 var time = 2000;
 
-var silder_content = document.getElementById('image_silder_wrapper');
+var slider_content = document.getElementById('image-slider-wrapper');
+var slider_image = document.getElementById('slideimage');
+var button = document.getElementsByTagName('button');
 
-images = document.getElementsByTagName('img');
-// console.log(images);
-// var imgwid = 
 
-// var img_width = images[i].offsetWidth;    
+var images= document.getElementsByTagName('img');
+
+
+var img_width = images[index].width;    
+var img_height = images[index].height;    
 var len = images.length;
-// console.log(len);
-// console.log(img_wi);
-// document.getElementById('image-slider-wrapper').width= len * img_width;
+var total_wid = len * img_width;
 
 
+var left = 0;
+var currentPos = 0;
 
 
+init(img_width,img_height);
 
+function init(x,y){
+    var container = document.querySelector('.main-carousal-container');
+    container.style.width= x+'px';
+    container.style.height= y+'px';
+    
+    container.style.position= "relative";
+    container.style.overflow= "hidden";
+    container.style.margin = 0+'px auto';
+    
+    
+    
+    slider_content.style.width= total_wid+'px';
+    slider_content.style.position="absolute";
+    slider_content.style.fontSize=0+'px';
+    slider_image.style.float="left";
+    // console.log(container);
+    // this.getButton(){
 
+    // }
 
-
-
-
-
-
-
-
-function draw(){
-    var dots= [];
-    var dotscontainer = document.getElementById('dots-container');
-
-    for(var i=0; i<len;i++){
-        var dot = document.createElement("span");
-        dot.classList.add("dots");
-        dotscontainer.append(dot);
-        dots.push(dot);
-    }
-    images[index].style.display ="block";
-
-    dots[index].classList.add("active");
-
-    // nextImage(index,len,dots,images);
 }
-draw();
+function slideImage(){
+    currentPos= left+ index * img_width;
+    console.log(currentPos);
+    slider_content.style.left=-currentPos+'px';
+    index++;    
 
-var currentindex=0;
-
-function slideImage(index){
-    index++;
-    // console.log(index);
-
-    images[index].style.display ="block";
-    images[index-1].style.display ="none";
-    console.log(index);
-    
-    
-    // console.log(len);
-    if(index >= len-1){
-        index= -1;
+    if(currentPos == total_wid- img_width){
+        index = 0;
     }
 }
-function changeimage(change){
-    
-    index = index + change;
-    // console.log(index);
-    images[index].style.display ="block";
-    images[index-1].style.display ="none";
-    dots[index].classList.add('active');
-    
-    console.log(index);
 
-    // console.log(len);
-    if(index >= len-1){
-        index= -1;
+
+setInterval(function(){
+    // slideImage();
+},3000);
+
+
+function changeimage(direction){
+    if(direction>0){
+        currentPos = currentPos + direction * img_width;
+        slider_content.style.left = -currentPos + 'px';
+        direction++;
+        console.log(currentPos);
+
+        if(currentPos >= total_wid - img_width){
+            currentPos = -380;
+        }
     }
-    else if(index <= -1){
-        images[len-2].style.display ="none";
-        console.log(index);
-    }
+    else{
+        currentPos = total_wid - index*img_width;
+        console.log("current position is: ", currentPos);
+        slider_content.style.left=-currentPos+'px';
+        index++;
+        if(currentPos<=0){
+            index = 1;
+        }
+        currentPos =0;
+        }
+
 }
-// setInterval(slideImage, 1000);
+
+// function draw(){
+//     var dots= [];
+//     var dotscontainer = document.getElementById('dots-container');
+
+//     for(var i=0; i<len;i++){
+//         var dot = document.createElement("span");
+//         dot.classList.add("dots");
+//         dotscontainer.append(dot);
+//         dots.push(dot);
+//     }
+//     images[index].style.display ="block";
+
+//     dots[index].classList.add("active");
+
+//     // nextImage(index,len,dots,images);
+// }
+// draw();
+
+// var currentindex=0;
+
+// function slideImage(index){
+//     index++;
+//     // console.log(index);
+
+//     images[index].style.display ="block";
+//     images[index-1].style.display ="none";
+//     console.log(index);
+    
+    
+//     // console.log(len);
+//     if(index >= len-1){
+//         index= -1;
+//     }
+// }
+// function changeimage(change){
+//     if(change>0){
+//         index = index + change;
+//         // console.log(index);
+//         images[index].style.display ="block";
+//         images[index-1].style.display ="none";
+//         // dots[index].classList.add('active');
+
+//         console.log(index);
+
+//         // console.log(len);
+//         if(index >= len-1){
+//             images[index].style.display ="none";
+//             index= -1;
+//         }
+
+//         // console.log(len);
+//         if(index == 0){
+//             index= len-1;
+//             console.log(index);
+
+//         }
+
+//     }
+// }
+// // setInterval(slideImage, 1000);
